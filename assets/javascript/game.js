6b586d01362fe509e6b016ba22441891cc3bce87
ann-leonard <aleo0779@gmail.com>
2019-02-22 13:25:59 -0500

    created reset function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,41 @@
-
-score=0
-buttons=[]
-for (i =1; i < 27; i++){
-  buttons.push(document.getElementsByClassName('btn')[i])
-}
-var wordList= ['starship','enterprise','klingon','vulcan','federation','tribble', 'computer','stardate' ]
-var word = wordList[Math.floor(Math.random()*wordList.length)];
-
-
-spaces=[]
-for (i=0;i<word.length;i++){
-  spaces.push('_')
-}
-
-document.getElementById('jumbo').innerHTML=spaces.join(' ')
+var domGuesses = document.getElementById("domGuesses");
+var domWinScore = document.getElementById('winNum');
+var score = 0;
+var guessesLeft = 8;
+var gameRunning = false;
+var guessedLetterBank = [];
+var incorrectLetterBank = [];
+var spaces;
+var wordList= ['starship','enterprise','klingon','vulcan','federation','tribble','spock','kirk','scotty','computer','stardate' ]
+var word;
 var letters=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var buttons
+//function to reset all stats but score, pick new word and create placeholders
+function newGame(){
+  gameRunning = true;
+  guessesLeft = 8;
+  guessedLetterBank = [];
+  incorrectLetterBank = [];
+  word = wordList[Math.floor(Math.random()*wordList.length)];
+  spaces=[]
+    for (i=0;i<word.length;i++){
+        spaces.push('_')
+      }
+  domGuesses.textContent = guessesLeft;
+  document.getElementById('jumbo').innerHTML=spaces.join(' ')
+  buttons=[]
+    for (i =1; i < 27; i++){
+      buttons.push(document.getElementsByClassName('btn')[i])
+    }
+  /*
+  I thought I would be able to do something like
+  buttons.style.backgroundColor = 'initial'
+  and just have it set back to default because the original color is built into the class name via bootstrap but nope
+  */
 
+}
+
+//Takes in letter pressed and sees if it's in the word
 function gameLogic(){
     index_of_letter=[]
     var userChoice = event.key;
@@ -35,24 +55,36 @@ function gameLogic(){
               }
 
               buttons[i].style.backgroundColor = 'green'
+
               for(k=0; k < index_of_letter.length;k++){
                 console.log(index_of_letter)
                 spaces[index_of_letter[k]]=userChoice
                 document.getElementById("jumbo").innerHTML = spaces.join(' ')
                 if (spaces.join('') === word){
                   score++
-                  document.getElementById('winNum').innerHTML = score
+                  domWinScore.innerHTML = score
                 }
               }
               break
             }
             else{
               buttons[i].style.backgroundColor = 'red';
+
             }
           }
         }
       }
 
    }
-document.onkeyup=gameLogic
-document.getElementById('jumbo').innerHTML=spaces.join(' ')
+
+function checkLose(letter){
+
+}
+
+function checkWin(){
+
+}
+
+
+document.getElementById("resetButton").onclick = function(){newGame()};
+document.onkeyup=gameLogic;
