86693226b9531974bd1a23e77d1d82b89af72669
ann-leonard <aleo0779@gmail.com>
2019-02-23 12:16:03 -0500

    guessesLeftCount not working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,6 +10,7 @@ var wordList= ['starship','enterprise','klingon','vulcan','federation','tribble'
 var word;
 var letters=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var buttons
+
 //function to reset all stats but score, pick new word and create placeholders
 function newGame(){
   gameRunning = true;
@@ -28,60 +29,85 @@ function newGame(){
       buttons.push(document.getElementsByClassName('btn')[i])
     }
     for (i =0; i < 27; i++){
-    buttons[i].style.backgroundColor = null;
+    (document.getElementsByClassName('btn')[i]).style.backgroundColor = null;
+  }
+}
+//does not work- needs to take 1 away from guessesLeft when user enters letter not in word
+function guessesLeftCount(userChoice){
+
+  for (i =8; i<word.length;i--){
+    console.log(userChoice)
+    console.log(word[i])
+      if (userChoice != word[i]){
+         --guessesLeft;
+          
+      return true;
+      break
+      }
+      else{
+        return false;
+      }
   }
 }
 
-//Takes in letter pressed and sees if it's in the word
-function gameLogic(){
-    index_of_letter=[]
-    var userChoice = event.key;
-    for (i=0; i<letters.length;i++){
-
-        if (userChoice === letters[i]){
-
-
-          for (j=0; j<word.length;j++){
-            if (word[j]===userChoice){
+function storeIndex(userChoice){
+  var index_of_letter=[];
+  for (l=0; l<word.length;l++){
+    if (word[l]===userChoice){
+        index_of_letter.push(l)
+    }userChoice
+  }guessesLeftCount
+  return index_of_letter;
+}
 
 
-              for (l=0; l<word.length;l++){
-                if (word[l]===userChoice){
-                    index_of_letter.push(l)
-                }
+function updateSpaces(userChoice){
+  for (i=0; i<letters.length;i++){
+
+      if (userChoice === letters[i]){
+        for (j=0; j<word.length;j++){
+          if (word[j]===userChoice){
+
+            index_of_letter=storeIndex(userChoice);
+
+
+            buttons[i].style.backgroundColor = 'green'
+
+            for(k=0; k < index_of_letter.length;k++){
+              spaces[index_of_letter[k]]=userChoice
+              document.getElementById("jumbo").innerHTML = spaces.join(' ')
+              if (spaces.join('') === word){
+                score++
+                domWinScore.innerHTML = score;
               }
-
-              buttons[i].style.backgroundColor = 'green'
-
-              for(k=0; k < index_of_letter.length;k++){
-                console.log(index_of_letter)
-                spaces[index_of_letter[k]]=userChoice
-                document.getElementById("jumbo").innerHTML = spaces.join(' ')
-                if (spaces.join('') === word){
-                  score++
-                  domWinScore.innerHTML = score
-                }
-              }
-              break
             }
-            else{
-              buttons[i].style.backgroundColor = 'red';
 
-            }
-          }
+            break
         }
+            else{
+                for (k=0; k<word.length;k++){
+                  if(word[k] != userChoice){
+                  buttons[i].style.backgroundColor = 'red';
+                  }
+                }
+            }
       }
-
-   }
-
-function checkLose(letter){
-
+    }
+  }
 }
 
-function checkWin(){
 
-}
+
+//Takes in letter pressed and sees if it's in the word
+function gameLogic(userChoice){
+  var userChoice = event.key; 
+    guessesLeftCount(userChoice);
+    updateSpaces(userChoice);
+    return userChoice;
+  }
+
 
 
 document.getElementById("resetButton").onclick = function(){newGame()};
 document.onkeyup=gameLogic;
+newGame();
